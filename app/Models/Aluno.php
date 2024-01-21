158fa9d69094edce931a898d4e943dfe0fe986d8
abraao <abraaobeiro@gmail.com>
2024-01-21 17:43:37 -0300

    definindo atributos virtuais para serializacao

--- app/Models/Aluno.php
+++ app/Models/Aluno.php
@@ -12,6 +12,11 @@ class Aluno extends Model
 
     protected $table = 'alunos';
 
+    /**
+     * Indica os campos que podem ser definidos via definição em massa
+     *
+     * @var array
+     */
     protected $fillable = [
         'nome',
         'nascimento',
@@ -19,17 +24,40 @@ class Aluno extends Model
         'turma_id'
     ];
 
+    /**
+     * Define as conversões no momento da serialização
+     *
+     * @var array
+     */
     protected $casts = ['nascimento' => 'date:d/m/Y'];
 
+    /**
+     * Define os campos não visiveis no momento da serialização
+     *
+     * @var array
+     */
     // protected $hidden = ['created_at', 'updated_at'];
 
+    /**
+     * Definie os campos visiveis no momento da serialização
+     *
+     * @var array
+     */
     protected $visible = [
         'id',
         'nome',
         'nascimento',
         'genero',
-        'turma_id'
+        'turma_id',
+        'aceito'
     ];
+
+    /**
+     * Define os acessores enviados na serialização
+     *
+     * @var array
+     */
+    protected $appends = ['aceito'];
 
     /**
      * Define a relação entre aluno e turma
@@ -38,5 +66,15 @@ class Aluno extends Model
      */
     public function turma(): BelongsTo {
         return $this->belongsTo(Turma::class);
+    }
+
+    /**
+     * Cria um atributo virtual chamado aceito
+     *
+     * @return string
+     */
+    public function getAceitoAttribute(): string
+    {
+        return $this->attributes['nascimento'] > '2001-01-01' ? 'aceito' : 'Não aceito';
     }
 }
