899138570dbb9dd9691c674b172c8baa00c06d8f
abraao <abraaobeiro@gmail.com>
2024-01-22 20:40:11 -0300

    utilizando resource para transformar uma collection

--- app/Http/Resources/AlunoResource.php
+++ app/Http/Resources/AlunoResource.php
@@ -14,7 +14,6 @@ class AlunoResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        // return parent::toArray($request);
         return [
             'nome' => $this->nome,
             'nascimento' => $this->nascimento,
